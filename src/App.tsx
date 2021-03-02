6ecf858424e532e3e7c93da5d4d4bd5929f0d07e
Gohikeco1 <gohikeco1@gmail.com>
2021-03-01 22:36:36 -0600

    add login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+
+const App = () => {
+	const [username, setUsername] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	const [successVisible, setSuccessVisible] = useState<boolean>(false);
+	const [failureVisible, setFailureVisible] = useState<boolean>(false);
+
+	const changeUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setUsername(e.currentTarget.value);
+	};
+	const changePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setPassword(e.currentTarget.value);
+	};
+
+	const showSuccess = () => {
+		setSuccessVisible(true);
+	};
+	const hideSuccess = () => {
+		setSuccessVisible(false);
+	};
+	const showFailure = () => {
+		setFailureVisible(true);
+	};
+	const hideFailure = () => {
+		setFailureVisible(false);
+	};
+
+	const onSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		const emailIsValid = emailRegex.test(username);
+		const passwordIsValid = passwordRegex.test(password);
+		if (emailIsValid && passwordIsValid) {
+			showSuccess();
+		} else {
+			showFailure();
+		}
+	};
+
+	return (
+		<form>
+			{successVisible ? (
+				<div>
+					Success!<button onClick={hideSuccess}>Close</button>
+				</div>
+			) : null}
+			{failureVisible ? (
+				<div>
+					NOPE!<button onClick={hideFailure}>Close</button>
+				</div>
+			) : null}
+			<input
+				type="text"
+				value={username}
+				placeholder="username"
+				onChange={changeUsername}
+			/>
+			<input
+				type="password"
+				value={password}
+				placeholder="password"
+				onChange={changePassword}
+			/>
+			<button type="submit" onClick={onSubmit}>
+				submit
+			</button>
+		</form>
+	);
+};
 
 export default App;
